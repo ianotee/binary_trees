113d126cad6f088ec4af217b90c2dedd4f586023
Planter44 <planterplanter777@gmail.com>
2023-11-28 13:09:27 -0800

    mains

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,33 +1,40 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left - This is  the binary tree that inserts  a node to the left.
- * @parent: The pointer that inserts the child.
- * @value: This is the value param of the structure.
- * Description -This is the  full descriptions od the node.
- * Return:a newly created node and or just give null if not found.
+ * binary_tree_insert_right - This is the node for inserting.
+ * @parent:The pointer to the node.
+ * @value: The value of the param.
+ * Description: The description  of the parent.
+ * Return: new or null.			
  */
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *begin_node;
-    
-	/*The below is an if statement*/
-	if (!parent)
+         /*This is is the conditional statement*/
+	if (parent == NULL)
 		return (NULL);
-	/*This is the malloc function that gives enough space.*/
-	begin_node = malloc(sizeof(binary_tree_t));
-	if (!begin_node)
+        /*This is the malloc function for allocating memory.*/
+	new_node = malloc(sizeof(binary_tree_t));
+	if (begin_node == NULL)
 		return (NULL);
-	/*This is the begin_node logic*/
-	begin_node->n = value;
-	begin_node->parent = parent;
-	begin_node->right = NULL;
-	begin_node->left = parent->left;
-	parent->left = begin_node;
-
-/*This is the  conditional statement */
-	if (begin_node->left)
+	/*Conditional statement.*/
+	if (parent->left == NULL)
+	{
+		parent->left = begin_node;
+		begin_node->parent = parent;
+	}
+	else
+	{
+		begin_node->left = parent->left;
+		parent->left = begin_node;
 		begin_node->left->parent = begin_node;
+		begin_node->parent = parent;
+	}
+	/*The pointers of the parent node*/
+	begin_node->left = NULL;
+	begin_node->right = NULL;
+	begin_node->n = value;
+
 	return (begin_node);
 }
