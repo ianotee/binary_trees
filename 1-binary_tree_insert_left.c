a8abe8cb4204e5b77278e6eeaa3adf24fe5cec36
Planter44 <planterplanter777@gmail.com>
2023-11-28 13:38:20 -0800

    mains

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,21 +10,21 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *Todo;
+	binary_tree_t *new;
 
 	if (!parent)
 		return (NULL);
 
-	Todo = malloc(sizeof(binary_tree_t));
-	if (!Todo)
+	new = malloc(sizeof(binary_tree_t));
+	if (!new)
 		return (NULL);
 
-	Todo->n = value;
-	Todo->parent = parent;
-	Todo->right = NULL;
-	Todo->left = parent->left;
-	parent->left = Todo;
-	if (Todo->left)
-		Todo->left->parent = Todo;
-	return (Todo);
+	new->n = value;
+	new->parent = parent;
+	new->right = NULL;
+	new->left = parent->left;
+	parent->left = new;
+	if (new->left)
+		new->left->parent = new;
+	return (new);
 }
