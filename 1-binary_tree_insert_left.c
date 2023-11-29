e4a5a74bd29fa0a3290b8edfbfdbae7a95c676b6
ianotee <oteeian500@gmail.com>
2023-11-29 01:17:07 -0800

    second command

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,33 +1,38 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left - This is  the binary tree that inserts  a node to the left.
- * @parent: The pointer that inserts the child.
- * @value: This is the value param of the structure.
- * Description -This is the  full descriptions od the node.
- * Return:a newly created node and or just give null if not found.
+ * binary_tree_insert_left - inserts a node as left child
+ * @parent: pointer to node to insert the left child in
+ * @value: value to store in the new node
+ * Return: pointer to created node or NULL
  */
-
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *Todo;
-    
-	/*The below is an if statement*/
-	if (!parent)
-		return (NULL);
-	/*This is the malloc function that gives enough space.*/
-	Todo = malloc(sizeof(binary_tree_t));
-	if (!Todo)
-		return (NULL);
-	/*This is the Todo logic*/
-	Todo->n = value;
-	Todo->parent = parent;
-	Todo->right = NULL;
-	Todo->left = parent->left;
-	parent->left = Todo;
+	binary_tree_t *new_node;
 
-/*This is the  conditional statement */
-	if (Todo->left)
-		Todo->left->parent = Todo;
-	return (Todo);
+	if (parent == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->left = NULL;
+	new_node->right = NULL;
+	new_node->n = value;
+
+	if (parent->left == NULL)
+	{
+		parent->left = new_node;
+		new_node->parent = parent;
+	}
+	else
+	{
+		new_node->left = parent->left;
+		parent->left = new_node;
+		new_node->left->parent = new_node;
+		new_node->parent = parent;
+	}
+
+	return (new_node);
 }
