c15bf4ece66923eb94a44763472e736791e0b82f
ianotee <oteeian500@gmail.com>
2023-11-30 09:18:26 -0800

    first commit

--- binary_trees.h
+++ binary_trees.h
@@ -10,6 +10,8 @@
 #include <math.h>
 #include <limits.h>
 
+
+
 /**
  * struct binary_tree_s - The binary tree below.
  * @n: The n param of the node.
@@ -26,17 +28,23 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
+size_t binary_tree_leaves(const binary_tree_t *tree);
+int binary_tree_is_root(const binary_tree_t *node);
+void binary_tree_delete(binary_tree_t *tree);
+int binary_tree_is_leaf(const binary_tree_t *node);
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
-void binary_tree_delete(binary_tree_t *tree);
-int binary_tree_is_leaf(const binary_tree_t *node);
-int binary_tree_is_root(const binary_tree_t *node);
-void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
-void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 size_t binary_tree_height(const binary_tree_t *tree);
 size_t binary_tree_depth(const binary_tree_t *tree);
 size_t binary_tree_size(const binary_tree_t *tree);
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
+size_t binary_tree_nodes(const binary_tree_t *tree);
+int binary_tree_balance(const binary_tree_t *tree);
+
 
 #endif
+
